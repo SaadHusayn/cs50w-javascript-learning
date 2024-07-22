6161fb2d966b3b7585db65e1667ab86623653dfe
SaadHusayn <rsaad5095@gmail.com>
2024-07-22 13:06:01 +0500

    Created a Currency Coverter by calling APIs

--- counter.js
+++ counter.js
@@ -1,13 +1,19 @@
-let counter = 0;
+if(!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
 
 function count(){
+    let counter = localStorage.getItem('counter')
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
 
+    /* 
     if(counter % 10 === 0){
         // template literal
         alert(`The counter is now ${counter}`);
     }
+    */
 }
 
 // considering functions as values is functional programming
@@ -16,6 +22,9 @@ function count(){
 // document.querySelector('button').onclick = count
 
 document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
     document.querySelector('button').onclick = count;
     // document.querySelector('button').addEventListener('click', count);
-})
+
+    // setInterval(count, 1000);
+});
